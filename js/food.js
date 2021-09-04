d035b2ee8a47d24af5c6cdc4b7fbfdad45ed5f28
Venus <evis1394@gmail.com>
2021-09-04 16:27:35 +0300

    forth commit

--- js/food.js
+++ js/food.js
@@ -44,7 +44,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 //timer 
 
-const deadline = '2021-08-02';
+const deadline = '2021-09-07';
 function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
 
@@ -189,40 +189,24 @@ function getZero(num) {
                     this.parent.append(element);
         }
     }
+    getResource(' http://localhost:3000/menu')
+    .then(data => 
+        { data.forEach(({img, altimg, title, descr, price}) =>{
+    new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
 
-            new MenuCard(
-                "img/tabs/vegy.jpg",
-                "vegy",
-                "'Меню 'Фитнес'",
-                "Меню 'Фитнес' это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством",
-                 30,
-                 ".menu .container",
-                 
-                 
-            ).render();
-            new MenuCard(
-                "img/tabs/elite.jpg",
-                "elite",
-                "'Меню “Премиум'",
-                "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-                 30,
-                 ".menu .container",
-                
-                 
-            ).render();
-            new MenuCard(
-                "img/tabs/post.jpg",
-                "post",
-                "'Меню 'Постное'",
-                "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков",
-                 30,
-                 ".menu .container",
-                
-                 
-            ).render();
-            
+});
+});
+        async function getResource ( url) {
+        let res =  await fetch(url);
+         if (!res.ok) {
+             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
 
-            // Forms
+         }
+        return await res.json();
+
+    };   
+         
+         // Forms
 
             const forms = document.querySelectorAll('form');
             
@@ -232,16 +216,25 @@ function getZero(num) {
                 failure: 'Попробуй снова',
             };
             forms.forEach(item => {
-                postData(item);
+                bindPostData(item);
 
             });
              
+            const postData =  async (url, data) => {
+                let res =  await fetch(url, {
+                method: 'POST', 
+                headers: { 'Content-type' : 'application/json' },
+                body: data
+                });
+                return await res.json();
+            };
 
-            function postData(form) {
+
+            function bindPostData(form) {
                 form.addEventListener('submit', (e) => {
                     e.preventDefault();
 
-                    const statusMessage = document.createElement('img');
+                    let statusMessage = document.createElement('img');
                     statusMessage.src = message.loading;
                     statusMessage.style.cssText = `
                     display: block;
@@ -250,21 +243,13 @@ function getZero(num) {
                  
 
                     const formData = new FormData(form);
-
-                    const object = {};
-                    formData.forEach(function(value, key) {
-                        object[key] = value;
-
-                    });
+                     
+                    const json = JSON.stringify(Object.fromEntries(formData.entries()));
                     
                     form.insertAdjacentElement('afterend', statusMessage); 
 
-                    fetch('server.php', {
-                        method: 'POST', 
-                        headers: { 'Content-type' : 'application/json' },
-                        body: JSON.stringify(object),
-                    })
-                    .then(data => data.text())
+
+                        postData('http://localhost:3000/requests', json)
                     .then(data=> {
                         console.log(data);
                         showThanksModal(message.success);
@@ -301,31 +286,6 @@ function getZero(num) {
                 
             }, 4000);
         }
-
         }); 
     }
-
-
-// command + shift + r - сбросить кэш
-
-
-        // // для метода get
- 
-        // fetch('https://jsonplaceholder.typicode.com/todos/1')
-        //         .then(response => response.json())
-        //         .then(json => console.log(json));
-
-        //  для метода post
-
-        // fetch('https://jsonplaceholder.typicode.com/posts', {
-
-        // method: 'POST',
-        // body: JSON.stringify({name: 'Alex'}),
-        // headers: {
-        //     'Content-type': 'application/json'
-        // }
-        //     })
-        //      .then(response => response.json())
-        //      .then(json => console.log(json));
-        //
-})
+});
