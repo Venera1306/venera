f21d6e908be4542f323ef09e578447c4ff6fc3d0
Venus <evis1394@gmail.com>
2021-09-05 15:32:29 +0300

    sixth commit slider

--- js/food.js
+++ js/food.js
@@ -210,7 +210,65 @@ axios.get('http://localhost:3000/menu')
         return await res.json();
 
     };   
-         
+    
+
+    // слайдер
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+
+
+        if (slides.length < 10) {
+            total.textContent = `0${slides.length}`;
+
+        } else {
+            total.textContent = slides.length; 
+        }
+
+    
+    let slideIndex = 1
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+        
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+ 
+        } else {
+            current.textContent = slideIndex; 
+        }
+
+        };
+
+
+       function plusSlides(n) {
+           showSlides(slideIndex += n);
+        }
+
+        prev.addEventListener('click', () => {
+        plusSlides(-1);
+    
+        });
+       
+        next.addEventListener('click', () => {
+            plusSlides(1);
+        });
+       
+    
+
+    
          // Forms
 
             const forms = document.querySelectorAll('form');
